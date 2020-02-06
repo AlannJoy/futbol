8bf3701c82f18b69be027fbd6441ca1f05d4d697
AlannJoy <alldaubeterre@gmail.com>
2020-02-06 14:25:16 +0100

    router

--- index.js
+++ index.js
@@ -2,26 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const db = require("./db");
-const Team = require("./team/model");
+// const Team = require("./team/model");
+const { teamRouter } = require("./team/router");
 // const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 
-const books = [
-  { id: 1, title: "To Kill a Mockingbird", author: "Harper Lee" },
-  {
-    id: 2,
-    title: "Harry Potter and the Sorcerer's Stone",
-    author: "J.K. Rowling"
-  },
-  { id: 3, title: "Pride and Prejudice", author: "Jane Austen" },
-  { id: 4, title: "The Diary of a Young Girl", author: "Anne Frank" }
-];
-app.get("/", (req, res) => res.redirect("/books"));
-app.get("/books", (req, res) => res.json({ data: books }));
-app.get("/books/:bookId", (req, res) => {
-  const bookId = req.params.bookId;
-  const book = books.find(b => b.id == bookId);
-  res.json(book);
-});
+app.use("/team", teamRouter);
 app.get("/", (req, res) => {
   db;
   res.json(db);
---
+++ team/router.js
@@ -0,0 +1,12 @@
+const { Router } = require("./express");
+const { Team } = require("./model");
+
+const router = new Router();
+
+router
+  .get("/", (req, res, next) => {
+    Team.findAll().then(teams => res.json(teams));
+  })
+  .catch(error => next(error));
+
+module.export = router;
