f4c1446206f804ac1970817ffd0c693ec63729f0
AlannJoy <alldaubeterre@gmail.com>
2020-02-06 05:39:34 +0100

    Sequelize Connected

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 4000;
 const db = require("./db");
 // const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
 
-app.get((req, res) => {
+app.get("/", (req, res) => {
   db;
   res.json(db);
 });
