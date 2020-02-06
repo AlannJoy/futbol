42700bdcd5bae0472dfc5d3fe084614648182b6c
AlannJoy <alldaubeterre@gmail.com>
2020-02-06 14:27:29 +0100

    router

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const db = require("./db");
-// const Team = require("./team/model");
+const Team = require("./team/model");
 const { teamRouter } = require("./team/router");
-// const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres'
+const databaseUrl =
+  process.env.DATABASE_URL ||
+  "postgres://postgres:secret@localhost:5432/postgres";
 
 app.use("/team", teamRouter);
 app.get("/", (req, res) => {
